261fddd9868cc07ca699a0f840c8a0cca699dcae
bishalninja <bishalprasad@ninjacart.com>
2026-07-05 17:08:44 +0530

    first push

---
+++ client/src/pages/Dashboard.tsx
@@ -0,0 +1,15 @@
+const Dashboard = () => {
+  return (
+    <div
+      style={{
+        padding: "40px",
+        fontSize: "30px",
+        fontWeight: "bold",
+      }}
+    >
+      🚛 Cost Estimator Dashboard
+    </div>
+  );
+};
+
+export default Dashboard;
